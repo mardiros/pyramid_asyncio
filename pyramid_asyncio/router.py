f1927048f977ac28ca086572e1293e92f3d31ff0
Guillaume Gauvrit <guillaume@gauvr.it>
2015-02-13 21:40:16 +0100

    Copy/Paste excview_tween_factory code in the router :/
    the excview is a function, so no exception are raised
    when handling the request as this time when its's
    a generator

--- pyramid_asyncio/router.py
+++ pyramid_asyncio/router.py
@@ -3,8 +3,8 @@ Override the Pyramid Router in order to get request handler as
 asyncio coroutine.
 """
 
+import sys
 import asyncio
-import inspect
 import logging
 
 from zope.interface import (
@@ -20,6 +20,7 @@ from pyramid.interfaces import (
     ITraverser,
     IView,
     IViewClassifier,
+    IExceptionViewClassifier,
     ITweens,
     )
 
@@ -231,7 +232,38 @@ class Router(RouterBase):
 
                 has_listeners and notify(NewResponse(request, response))
 
-                return response
+            # XXX excview_tween_factory is not a generator
+            # Move a part of its code here
+            except Exception as exc:
+                # WARNING: do not assign the result of sys.exc_info() to a local
+                # var here, doing so will cause a leak.  We used to actually
+                # explicitly delete both "exception" and "exc_info" from ``attrs``
+                # in a ``finally:`` clause below, but now we do not because these
+                # attributes are useful to upstream tweens.  This actually still
+                # apparently causes a reference cycle, but it is broken
+                # successfully by the garbage collector (see
+                # https://github.com/Pylons/pyramid/issues/1223).
+                attrs= request.__dict__
+                attrs['exc_info'] = sys.exc_info()
+                attrs['exception'] = exc
+                adapters = request.registry.adapters
+
+                # clear old generated request.response, if any; it may
+                # have been mutated by the view, and its state is not
+                # sane (e.g. caching headers)
+                if 'response' in attrs:
+                    del attrs['response']
+                # we use .get instead of .__getitem__ below due to
+                # https://github.com/Pylons/pyramid/issues/700
+                request_iface = attrs.get('request_iface', IRequest)
+                provides = providedBy(exc)
+                for_ = (IExceptionViewClassifier, request_iface.combined, provides)
+                view_callable = adapters.lookup(for_, IView, default=None)
+                if view_callable is None:
+                    raise
+                response = view_callable(exc, request)
+                while is_generator(response):
+                    response = yield from response
 
             finally:
                 if request.finished_callbacks:
@@ -239,6 +271,9 @@ class Router(RouterBase):
 
         finally:
             manager.pop()
+
+        return response
+
 
     @asyncio.coroutine
     def __call__(self, environ, start_response):
